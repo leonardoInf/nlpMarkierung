040d3770092ada2e64cacdce29b51ccef1e4704a
leonardoInf <mondloslp@gmail.com>
2020-10-12 09:47:39 +0200

    oo

---
+++ frontend/src/components/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Container, Menu } from "semantic-ui-react";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Start from "./Start";
+import Impressum from "./Impressum";
+import GrayBackground from "../assets/background.jpg";
+import "./styles/App.css";
+
+export default class App extends React.Component {
+  state = { background: `url(${GrayBackground})` };
+  restoreBackground = () =>
+    this.setState({ background: `url(${GrayBackground})` });
+  clearBackground = () => this.setState({ background: "none" });
+
+  render() {
+    return (
+      <div
+        className="background"
+        style={{ backgroundImage: this.state.background }}
+      >
+        <Container style={{ width: "70%" }}>
+          <BrowserRouter>
+            <Menu>
+              <Menu.Item>
+                <a href="https://braguinski.de" style={{color: "black"}}>Startseite</a>
+              </Menu.Item>
+              <Menu.Item as={Link} to="/" onClick={this.restoreBackground}>
+                App
+              </Menu.Item>
+              <Menu.Item
+                as={Link}
+                to="/impressum"
+                onClick={this.clearBackground}
+              >
+                Impressum
+              </Menu.Item>
+            </Menu>
+            <Switch>
+              <Route exact path="/">
+                <Start />
+              </Route>
+              <Route path="/impressum">
+                <Impressum />
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </Container>
+      </div>
+    );
+  }
+}
---
+++ frontend/src/components/Start.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Divider,
+  Form,
+  Grid,
+  Icon,
+  Segment,
+  TextArea,
+} from "semantic-ui-react";
+import "./styles/Start.css";
+
+export default function Start() {
+  return (
+    <div>
+      <h1 className="titel">
+        Automatische Markierung von Texten
+      </h1>
+      <Segment>
+        <Grid columns={2} relaxed="very" verticalAlign="middle">
+          <Grid.Column>
+            <Form>
+              <TextArea
+                placeholder="Geben Sie hier einen Text ein"
+                className="AppInput"
+              />
+            </Form>
+          </Grid.Column>
+          <Grid.Column>
+            <Form>
+              <TextArea
+                readOnly
+                placeholder="Hier wird der markierte Text erscheinen"
+                className="AppInput"
+              />
+            </Form>
+          </Grid.Column>
+        </Grid>
+        <Divider vertical>
+          <Icon name="arrow circle right"></Icon>
+        </Divider>
+      </Segment>
+    </div>
+  );
+}
